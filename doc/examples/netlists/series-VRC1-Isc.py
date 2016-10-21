53643c97136069532058675c53d9cd75049916f5
Michael Hayes <michael.hayes@canterbury.ac.nz>
2016-10-21 17:19:47 +1300

    Improve detection of IVP for network

--- doc/examples/netlists/series-VRC1-Isc.py
+++ doc/examples/netlists/series-VRC1-Isc.py
@@ -1,12 +1,12 @@
 from lcapy import *
-from numpy import logspace
+from numpy import linspace
 from matplotlib.pyplot import savefig, show
 
-N = Vdc(10) + R(10) + C(1e-4)
+N = Vdc(20) + R(10) + C(1e-4)
 
-vf = logspace(0, 5, 400)
-N.Isc.frequency_response().plot(vf, log_scale=True)
+tv = linspace(0, 0.01, 1000)
+N.Isc.transient_response().plot(tv)
 
 show()
 
-savefig('series-VRC1-Isc.png')
+savefig('series-VRC1-isc.png')
