fd2b84fb32cc659ae708dc7702094a3103fa3c22
Michael Hayes <michael.hayes@canterbury.ac.nz>
2017-08-09 18:43:19 +1200

    Remove initial condition from test

--- doc/examples/netlists/series-VRC1-isc.py
+++ doc/examples/netlists/series-VRC1-isc.py
@@ -1,12 +1,12 @@
 from lcapy import *
-from numpy import linspace
+from numpy import logspace
 from matplotlib.pyplot import savefig, show
 
-N = Vstep(20) + R(10) + C(1e-4, 0)
+N = Vstep(10) + R(10) + C(1e-4)
 
-tv = linspace(0, 0.01, 1000)
-N.Isc.transient_response().plot(tv)
+vf = logspace(0, 5, 400)
+N.Isc.frequency_response().plot(vf, log_scale=True)
 
 show()
 
-savefig('series-VRC1-isc.png')
+savefig('series-VRC1-Isc.png')
