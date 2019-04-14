05e2f487a8c8efecdea4af9778520189457f838e
Michael Hayes <michael.hayes@canterbury.ac.nz>
2019-04-14 14:31:19 +1200

    Update networks examples

--- doc/examples/networks/series-VRC1-isc.py
+++ doc/examples/networks/series-VRC1-isc.py
@@ -2,9 +2,9 @@ from lcapy import *
 from numpy import linspace
 from matplotlib.pyplot import savefig
 
-N = Vstep(20) + R(10) + L(1e-2, 0)
+N = Vstep(20) + R(10) + C(1e-4, 0)
 
 vt = linspace(0, 0.01, 1000)
 N.Isc(t).plot(vt)
 
-savefig('series-VRL1-isc.png')
+savefig('series-VRC1-isc.png')
