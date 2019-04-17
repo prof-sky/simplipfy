590b481c63e9c0065d1035c7e7c992a35c3946ec
Michael Hayes <michael.hayes@canterbury.ac.nz>
2019-04-17 22:46:04 +1200

    Add preliminary nodal analysis

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='lcapy',
-      version='0.33.5',
+      version='0.33.1',
       author='Michael Hayes',
       author_email='michael.hayes@canterbury.ac.nz',
       description='Symbolic linear circuit analysis',
@@ -14,10 +14,12 @@ setup(name='lcapy',
       url='https://github.com/mph-/lcapy',
       download_url='https://github.com/mph-/lcapy',
       # Some of these requirements could be relaxed.
-      install_requires=['matplotlib',
-                        'scipy',
-                        'numpy',                        
-                        'sympy'],
+      install_requires=['numpy >= 1.12',
+                        'matplotlib >= 2.2.2',
+                        'scipy >= 1.2.0',
+                        'sympy >= 1.3',
+                        'networkx',
+      ],
       packages=find_packages(),
       py_modules=['schtex'],       
       entry_points={
