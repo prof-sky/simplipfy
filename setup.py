5712fffc46b6e8b69e6c82dff4883aab5097ecf2
Michael Hayes <michael.hayes@canterbury.ac.nz>
2019-04-22 11:50:14 +1200

    V0.34.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='lcapy',
-      version='0.33.1',
+      version='0.34.0',
       author='Michael Hayes',
       author_email='michael.hayes@canterbury.ac.nz',
       description='Symbolic linear circuit analysis',
@@ -14,10 +14,10 @@ setup(name='lcapy',
       url='https://github.com/mph-/lcapy',
       download_url='https://github.com/mph-/lcapy',
       # Some of these requirements could be relaxed.
-      install_requires=['numpy >= 1.12',
-                        'matplotlib >= 2.2.2',
-                        'scipy >= 1.2.0',
-                        'sympy >= 1.3',
+      install_requires=['matplotlib',
+                        'scipy',
+                        'numpy',                        
+                        'sympy',
                         'networkx',
       ],
       packages=find_packages(),
