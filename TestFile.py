c71fcecb40214bc861548cf8bc4b52e1196fed40
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2024-04-23 09:35:49 +0200

    added parallel resistor

--- TestFile.py
+++ TestFile.py
@@ -8,6 +8,9 @@ cct = Circuit("""
 ... R2 5 6 {R2}; down
 ... R3 6 7 {R3}; down
 ... R4 7 8 {R4}; down
+... W6 7 11; right
+... R5 11 12 {R5}; down
+... W7 12 8; left
 ... W3 8 9; left
 ... W4 9 10; up
 ... W5 10 0; up
