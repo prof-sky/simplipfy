cf0b03acf62cde0e552005528781713e86a411e9
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2024-04-24 16:06:27 +0200

    moved netlist from py-file to txt

--- Non-Lacapy-Files/TestFile.py
+++ Non-Lacapy-Files/TestFile.py
@@ -1,5 +1,5 @@
 from lcapy import Circuit
-
+import to_impedance
 
 def process_entry(cir: Circuit, iteration: int):
     print("-------------------------")
@@ -10,31 +10,9 @@ def process_entry(cir: Circuit, iteration: int):
     cir.draw()
 
 
-cct = Circuit("""
-... V1 0 2 dc {10}; up
-... W1 2 3; up
-... W2 3 4; right
-... R1 4 5 {100}; down
-... R2 5 6 {100}; down
-... R3 6 7 {100}; down
-... R4 7 8 {100}; down
-... W6 7 11; right
-... R5 11 12 {100}; down
-... W7 12 8; left
-... W3 8 9; left
-... W4 9 10; up
-... W5 10 0; up
-""")
-
-print("--- Currents ---")
-print(cct.branch_currents())
-
-print("--- Voltages ---")
-print(cct.branch_voltages())
-
-print("--- Resistance ---")
-print(cct.resistance(4, 5))
-print(cct.netlist())
+# cct = Circuit(to_impedance.ConvertNetlistFile("Circuit.txt"))
+cct = Circuit(open("ImpedaceCircuit.txt").read())
+cct2, _ = cct.simplify()
 
 cct.draw()
 cct2, nos = cct.simplify()
