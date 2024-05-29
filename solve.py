8208e9934fa3db35dbff629cf72af5fa064146db
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2024-05-29 11:19:42 +0200

    updated solve.py

--- solve.py
+++ solve.py
@@ -1,21 +1,15 @@
-print("started solve.py")
-
 from lcapy import Circuit
 from lcapy import Solution
 from lcapy import FileToImpedance
-import os
-print("imports  done")
 
 # cct = Circuit(FileToImpedance("Circuits/Circuit_mixed.txt"))
 # cct = Circuit(filename="Circuits/Circuit_resistors.txt")
 # cct = Circuit(filename="Circuits/Circuit_capacitors.txt")
-cct = Circuit(filename="Circuits/Circuit_inductors.txt")
+# cct = Circuit(filename="Circuits/Circuit_inductors.txt")
 
-steps = cct.simplify_stepwise()
-sol = Solution(steps)
-sol.draw(path="Solutions")
-sol.export(path="Solutions")
-
-print(os.listdir("Solutions"))
-
-print("finished solve.py")
+def solve_circuit(filename: str, filePath = "Circuits/"):
+    cct = Circuit(FileToImpedance(filePath+filename))
+    steps = cct.simplify_stepwise()
+    sol = Solution(steps)
+    sol.draw(path="Solutions")
+    sol.export(path="Solutions")
