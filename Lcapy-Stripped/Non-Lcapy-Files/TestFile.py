cdf289648f44243a5cb6857ab41fba5dd794a3e8
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2024-06-05 18:23:42 +0200

    test after merge

--- Lcapy-Stripped/Non-Lcapy-Files/TestFile.py
+++ Lcapy-Stripped/Non-Lcapy-Files/TestFile.py
@@ -2,15 +2,15 @@ from lcapy import Circuit
 from lcapy import Solution
 from lcapy import FileToImpedance
 
-# filename = "Non-Lcapy-Files/Circuit_inductors.txt"
-# filename = "Non-Lcapy-Files/Circuit_resistors.txt"
-# filename = "Non-Lcapy-Files/Circuit_capacitors.txt"
-filename = "Non-Lcapy-Files/Circuit_mixed.txt"
+# filename = "NCircuit_inductors.txt"
+# filename = "Circuit_resistors.txt"
+# filename = "Circuit_capacitors.txt"
+filename = "Circuit_mixed.txt"
 
 cct = Circuit(FileToImpedance(filename))
 
 steps = cct.simplify_stepwise()
 sol = Solution(steps)
-sol.draw(path="Non-Lcapy-Files/Solutions")
-sol.export(path="Non-Lcapy-Files/Solutions")
+sol.draw(path="Solutions")
+sol.export(path="Solutions")
 
