9d365a1212e9547878c3ce64818f345a2273890c
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2024-06-06 15:03:27 +0200

    powershell skripts for integraion in webstorm

---
+++ StartServerProcess.ps1
@@ -0,0 +1,11 @@
+if (Test-Path "server.pid"){
+    try {
+        Start-Process ./StopServerProcess.ps1
+    }
+    catch {
+        Write-Output "Process does not exist anymore, delting file"
+        Remove-Item "server.pid"
+    }
+}
+$serverProcess = Start-Process .\StartServer.ps1 -PassThru
+$serverProcess.Id | Out-File -FilePath "server.pid"
---
+++ StopServerProcess.ps1
@@ -0,0 +1,10 @@
+if (Test-Path "server.pid"){
+    $serverProcessId = Get-Content -Path "server.pid"
+    $process = Get-Process -Id $serverProcessId -ErrorAction SilentlyContinue
+    $childProcesses = Get-CimInstance Win32_Process | Where-Object { $_.ParentProcessId -eq $serverProcessId }
+    foreach ($child in $childProcesses) {
+        Stop-Process -Id $child.ProcessId -Force
+    }
+    Stop-Process -Id $process.Id
+    Remove-Item "server.pid"
+}
