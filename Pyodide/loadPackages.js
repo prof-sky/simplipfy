8743e46c2ba9ea1b231cdf6609e75d7b111a32c7
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2024-07-17 10:07:43 +0200

    move load packages in own file

---
+++ Pyodide/loadPackages.js
@@ -0,0 +1,31 @@
+async function load_packages(pyodide, optAddNames) {
+    createProgressBar("Loading Pyodide packages, please wait ...");
+    updateProgressBar(0);
+    let packageAddress = serverAddress + "/Packages/";
+    let packages = await fetchDirectoryListing(packageAddress, ".whl");
+
+    if(Array.isArray(optAddNames)){
+        for(let i = 0; i < optAddNames.length; i++){
+            packages.push(optAddNames[i]);
+        }
+    }
+
+    let progress = 0;
+    const updateProgress = () => {
+        progress += 1;
+        updateProgressBar(Math.floor((progress / packages.length) * 100));
+    };
+
+    let packagePromises = packages.map(async function (packageName) {
+        let pkgArrBuff = await (await fetch(serverAddress + "/Packages/" + packageName)).arrayBuffer();
+        let packageExtension = packageName.slice(packageName.lastIndexOf("."), packageName.length);
+        await pyodide.unpackArchive(pkgArrBuff, packageExtension);
+        updateProgress();
+    });
+
+    await Promise.all(packagePromises);
+    console.log("Installed:" + packages);
+    removeProgressBar();
+    pyodideReady = true;
+    document.getElementById('start-button').disabled = false;
+}
