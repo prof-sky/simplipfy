25c1cec0d8a0283ae813b25bf53c2a49482b2154
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2024-10-28 16:04:12 +0100

    start server in the right location

--- Pyodide/Scripts/StartServer.ps1
+++ Pyodide/Scripts/StartServer.ps1
@@ -1,3 +1,4 @@
 Write-Output "--- Pyodide-Core server with GZIP compression ---"
 Set-Location $(Split-Path -Path $MyInvocation.MyCommand.Path -Parent)
-python ..\GzipSimplePythonHttpServer.py
+Set-Location "..\"
+python GzipSimplePythonHttpServer.py
