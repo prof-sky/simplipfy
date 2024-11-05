69f84e3462210cfd8861ce04c3badf78054e5596
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2024-11-05 08:54:46 +0100

    remove some print statements in gitlab ci cd

--- Pyodide/Scripts/copyFiles.sh
+++ Pyodide/Scripts/copyFiles.sh
@@ -1,8 +1,6 @@
     ls -a
     echo "remove files from directory"
     find . -mindepth 1 -not -path "./.git*" -delete
-    echo "files after deletion"
-    ls -a
     cd ..
     echo "copy files"
     echo -e ".git/\n.idea/\nCircuits.zip\nScripts/\nConfigurations\nGzipSimplePythonHttpServer.py" > exclude-list.txt
@@ -10,5 +8,3 @@
     rm exclude-list.txt
     cd temp
     zip -r Circuits Circuits && rm -r Circuits/
-    echo "show copied files in temp"
-    ls -a
