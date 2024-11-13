421d8380a09ed2b9aa07cc22f9623fe202e4f281
grosshol <grosshol@hs-pforzheim.de>
2024-11-13 10:29:02 +0100

    Add letter to clearly see on phone when updated

--- Pyodide/src/scripts/languages/lang.en.js
+++ Pyodide/src/scripts/languages/lang.en.js
@@ -2,7 +2,7 @@ const english = {
     // Landing page text
     landingPageGreeting:
         "a free browser tool for learning<br>" +
-        "how to simplify electrical circuits",
+        "how to simplify electrical circuits A",
     selectorWaitingNote:
         "This may take a minute",
 
