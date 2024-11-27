2929900fe2bc5d1aa04ffade41b5d9e3987bd323
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2024-11-27 14:32:58 +0100

    correct path

--- Pyodide/Scripts/clearFTPServer.sh
+++ Pyodide/Scripts/clearFTPServer.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-FTP_SERVER="ftp://w0182f79.kasserver.com/simplipfy"
+FTP_SERVER_FULL_PATH="ftp://w0182f79.kasserver.com/simplipfy"
+FTP_SERVER="w0182f79.kasserver.com"
+FTP_FOLDER="/simplipfy"
 FTP_USER="f017019e"
 FTP_PASS="EuVumvJ4TyqztDz7Qgwq"
 
 # get all files and directories from ftp server
-ncftpls -u $FTP_USER -p $FTP_PASS $FTP_SERVER > removeFiles.txt
+ncftpls -u $FTP_USER -p $FTP_PASS $FTP_SERVER_FULL_PATH > removeFiles.txt
 # exclude index.html, matomo, Simplipfy
 sed -i '/index\.html/d' removeFiles.txt
 sed -i '/matomo/d' removeFiles.txt
@@ -19,6 +21,6 @@ done < removeFiles.txt
 rm removeFiles.txt
 
 # execute commands to clear ftp server
-ncftp -u $FTP_USER -p $FTP_PASS $FTP_SERVER <<EOF
+ncftp -u $FTP_USER -p $FTP_PASS $FTP_SERVER $FTP_FOLDER <<EOF
       $modified_content
 EOF
