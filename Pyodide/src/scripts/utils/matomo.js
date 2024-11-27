b2e51f26d2dffb0204c8c50557105d1ff689a234
grosshol <grosshol@hs-pforzheim.de>
2024-11-27 14:48:59 +0100

    rename file

--- Pyodide/src/scripts/utils/matomo.js
+++
@@ -1,6 +0,0 @@
-function pushPageViewMatomo(title="") {
-  if (typeof _paq !== 'undefined') {
-    _paq.push(['setDocumentTitle', document.title + "/" + title]);
-    _paq.push(['trackPageView']);
-  }
-}
