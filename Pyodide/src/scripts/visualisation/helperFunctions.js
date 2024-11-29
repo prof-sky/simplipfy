d2dbc639630163e69ffa7948b074825d35d825ee
grosshol <grosshol@hs-pforzheim.de>
2024-11-29 13:03:19 +0100

    move some functions to make main easier

--- Pyodide/src/scripts/visualisation/helperFunctions.js
+++ Pyodide/src/scripts/visualisation/helperFunctions.js
@@ -299,7 +299,49 @@ function whenAvailable(name, callback) {
     }, interval);
 }
 
+async function solveCircuit(circuitMap, pyodide) {
+    await clearSolutionsDir(pyodide);
 
+    let paramMap = new Map();
+    paramMap.set("volt", languageManager.currentLang.voltageSymbol);
+    paramMap.set("total", languageManager.currentLang.totalSuffix);
+
+    stepSolve = state.solve.SolveInUserOrder(
+        circuitMap.circuitFile,
+        `${conf.pyodideCircuitPath}/${circuitMap.sourceDir}`,
+        `${conf.pyodideSolutionsPath}/`,
+        paramMap);
+    await stepSolve.createStep0().toJs();
+
+    // Get information which components are used in this circuit
+    const componentTypes = await getCircuitComponentTypes(pyodide);
+
+    await getJsonAndSvgStepFiles(pyodide);
+    let stepDetails = fillStepDetailsObject(circuitMap, componentTypes);
+
+    display_step(pyodide, stepDetails);
+}
+
+function startSolving(pyodide) {
+    solveCircuit(state.currentCircuitMap, pyodide);
+    //The div element that contains the SVG representation of the circuit diagram.
+    const svgDiv = document.querySelector('.svg-container');
+    //The div element that contains the list of elements that have been clicked or selected in the circuit diagram.
+    const nextElementsContainer = document.querySelector('.next-elements-container');
+    if (svgDiv && nextElementsContainer) {
+        resetNextElements(svgDiv, nextElementsContainer);
+    }
+}
+
+function fillStepDetailsObject(circuitMap, componentTypes) {
+    let stepDetails = new StepDetails;
+    stepDetails.showVCButton = circuitIsNotSubstituteCircuit(circuitMap);
+    stepDetails.jsonZPath = `${conf.pyodideSolutionsPath}/${state.jsonFiles_Z[state.currentStep]}`;
+    stepDetails.jsonZVCath = (state.jsonFiles_VC === null) ? null : `${conf.pyodideSolutionsPath}/${state.jsonFiles_VC[state.currentStep]}`;
+    stepDetails.svgPath = `${conf.pyodideSolutionsPath}/${state.svgFiles[state.currentStep]}`;
+    stepDetails.componentTypes = componentTypes;
+    return stepDetails;
+}
 
 
 
