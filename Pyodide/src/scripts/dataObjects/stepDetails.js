448390ccac4e6469938d5d7e1f2f9d21d66ae6a9
grosshol <grosshol@hs-pforzheim.de>
2024-12-12 12:02:10 +0100

    make circuitInfo available in step

--- Pyodide/src/scripts/dataObjects/stepDetails.js
+++ Pyodide/src/scripts/dataObjects/stepDetails.js
@@ -1,9 +1,25 @@
 class StepDetails {
-    constructor(showVC, jsonZPath, jsonVCPath, svgPath, componentTypes) {
+    constructor(showVC, jsonZPath, jsonVCPath, svgPath, circuitInfo) {
         this.showVCData = showVC;
         this.jsonZPath = jsonZPath;
         this.jsonVCPath = jsonVCPath;
         this.svgPath = svgPath;
-        this.componentTypes = componentTypes;
+        this.circuitInfo = circuitInfo;
+    }
+
+    getComponentTypes() {
+        // TODO Add check
+        return this.circuitInfo["componentTypes"];
+    }
+    getOmegaVal() {
+        return this.circuitInfo["omega_0"];
+    }
+    getElementNamesAndValues() {
+        // Return source and all elements
+        let obj = structuredClone(this.circuitInfo);
+        delete obj["omega_0"];
+        delete obj["componentTypes"];
+        return obj;
+
     }
 }
