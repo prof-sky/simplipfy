631a0b61b253d87f2242e5997ffeae95d14ab908
grosshol <grosshol@hs-pforzheim.de>
2024-12-13 10:18:56 +0100

    define toggle symbol, remove buttons in cloned svg, make sure names are shown in cloned svg

--- Pyodide/src/scripts/visualisation/helperFunctions.js
+++ Pyodide/src/scripts/visualisation/helperFunctions.js
@@ -346,6 +346,9 @@ function fillStepDetailsObject(circuitMap, circuitInfo) {
 }
 
 function getCheckBoxValue(circuitMap) {
+    if (circuitMap.selectorGroup === circuitMapper.selectorIds.quick) {
+        return true; // Definition of what the quickstart does show, make false if no VC wished here
+    }
     // only check the checkbox corresponding to the current circuit group
     const checkBox = document.getElementById(`${circuitMap.selectorGroup}-showVCData`);
     return checkBox.checked;
