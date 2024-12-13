d3a42bd8eeb8b13a8ccd80c1f77d20ae98b43027
grosshol <grosshol@hs-pforzheim.de>
2024-12-13 12:11:40 +0100

    add allowedDirNames

---
+++ Pyodide/src/scripts/definitions/allowedCircuitDirectories.js
@@ -0,0 +1,10 @@
+// This is to define which directories are allowed to be used for circuits
+// It could be anything but it's good to check it because maybe some have different things to show and need to be adapted
+
+const allowedDirNames = {
+    quickstart: "quickstart",
+    resistor: "resistor",
+    capacitor: "capacitor",
+    inductor: "inductor",
+    mixed: "mixed",
+}
