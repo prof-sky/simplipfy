bf635b838c909ea2283bb711d06febd2a0f9dad1
grosshol <grosshol@hs-pforzheim.de>
2025-01-20 07:18:17 +0100

    Show symbols by default

--- Pyodide/src/scripts/visualisation/pageManager.js
+++ Pyodide/src/scripts/visualisation/pageManager.js
@@ -24,6 +24,7 @@ class PageManager {
     }
 
     showSelectPage() {
+        state.valuesShown = false; // by default symbols shown
         this.landingPage.style.display = "none";
         this.selectPage.style.display = "block";
         this.simplifierPage.style.display = "none";
