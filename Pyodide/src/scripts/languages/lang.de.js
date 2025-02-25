7a69de12cff04ebe98ce84b8210528df010d3018
grosshol <grosshol@hs-pforzheim.de>
2025-02-25 14:08:36 +0100

    add licenses and package to about page

--- Pyodide/src/scripts/languages/lang.de.js
+++ Pyodide/src/scripts/languages/lang.de.js
@@ -174,5 +174,13 @@ const german = {
         "<br><br>simplipfy wird von der Hochschule Pforzheim betrieben.<br>" +
         "Die Software wird unter der Leitung von Prof.Dr. Stefan Kray entwickelt und von der Stiftung Innovation in der Hochschullehre gef&oumlrdert.<br>" +
         "Das Tool soll Studierenden und Schülern beim Erlernen von Elektrotechnik-Themen helfen.<br><br>" +
-        "Das Projekt ist open-source und kann unter folgendem Link auf GitHub eingesehen werden: <a href='https://github.com/prof-sky/simplipfy'>Github-Repo</a>"
+        "Das Projekt ist open-source und kann unter folgendem Link auf GitHub eingesehen werden: <a href='https://github.com/prof-sky/simplipfy'>Github-Repo</a><br><br>" +
+        "<h3>Benutzte Bibliotheken</h3>" +
+        "<ul>" +
+        "<li><a href='https://github.com/prof-sky/lcapy/tree/lcapy-inskale'>Lcapy (adaptiert)</a> [LGPL-2.1 Lizenz]</li>" +
+        "<li><a href='https://github.com/cdelker/schemdraw'>SchemDraw (adaptiert)</a> [MIT Lizenz]</li>" +
+        "<li><a href='https://github.com/pyodide/pyodide'>Pyodide</a> [MPL-2.0 Lizenz]</li>" +
+        "</ul>"
+
+
 }
