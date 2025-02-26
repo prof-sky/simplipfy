bc3da6605002bb88c0e6fddf0d8445c3c3ab8034
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2025-02-26 08:53:42 +0100

    remove server Adress and access token, push without history

--- Pyodide/Scripts/clearFTPServer.sh
+++ Pyodide/Scripts/clearFTPServer.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-while getopts "u:p:" flag
-    do
-             case "${flag}" in
-                    u) FTP_USER=${OPTARG};;
-                    p) FTP_PASS=${OPTARG};;
-             esac
-    done
-
-FTP_SERVER_FULL_PATH="ftp://w0182f79.kasserver.com/simplipfy"
-FTP_SERVER="w0182f79.kasserver.com"
-FTP_FOLDER="/simplipfy"
-
 # get all files and directories from ftp server
 ncftpls -u $FTP_USER -p $FTP_PASS $FTP_SERVER_FULL_PATH > removeFiles.txt
 # exclude index.html, matomo, Simplipfy
