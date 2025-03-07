46c6fbfe10666dac8aa43727ea7a31ec150b82b4
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2025-03-07 11:57:41 +0100

    fixed github test address and added visualisation for error while loading
    commit 804abdf06d2bb4f184d82cec3f59473c80ea11c8
    Author: yannick.wieland <yannick.wieland@hs-pforzheim.de>
    Date:   Fri Mar 7 10:35:23 2025 +0100
    
        remove test error
    
    commit 7bb612c9d7dfeea71cf3194bf83f8ab69e3a0e41
    Author: yannick.wieland <yannick.wieland@hs-pforzheim.de>
    Date:   Fri Mar 7 10:33:25 2025 +0100
    
        indicate error while loading
    
    commit e895cee82f52fe9f57a3210919291c24afa1e703
    Author: yannick.wieland <yannick.wieland@hs-pforzheim.de>
    Date:   Fri Mar 7 08:47:46 2025 +0100
    
        adjust path

--- Pyodide/src/scripts/utils/packageManager.js
+++ Pyodide/src/scripts/utils/packageManager.js
@@ -21,9 +21,15 @@ class PackageManager {
     }
 
     async doLoadsAndImports() {
-        await this.loadCircuits();
-        await this.importPyodidePackages();
-        await this.importSolverModule();
+        try{
+            await this.loadCircuits();
+            await this.importPyodidePackages();
+            await this.importSolverModule();
+        }
+        catch (error){
+            this.onError();
+            throw new Error(error)
+        }
     }
 
     async loadCircuits() {
@@ -130,6 +136,7 @@ class PackageManager {
             return fileNames;
         } catch (error) {
             console.error('Error fetching directory listing:', error);
+            this.onError();
             return [];
         }
     }
@@ -137,7 +144,7 @@ class PackageManager {
     async #fetchGitHubDirectoryContents(path, extension) {
 
         let url = `https://api.github.com/repos/${conf.gitHubUser}${conf.gitHubProject}contents/${path}`;
-        if (!(await fetch(url+"/.htaccess")).ok) {
+        if (!(await fetch(url+".htaccess")).ok) {
             url = `https://api.github.com/repos/${conf.gitHubUser}${conf.gitHubProject}contents/Pyodide/${path}`;
         }
 
@@ -154,7 +161,17 @@ class PackageManager {
             return data.filter(file => file.name.endsWith(extension)).map(file => file.name);
         } catch (error) {
             console.error('Error fetching GitHub directory contents:', error);
+            this.onError();
             return [];
         }
+    }
+
+    onError() {
+        let progressBar = document.getElementById('pgr-bar')
+        progressBar.classList.remove('bg-warning');
+        progressBar.classList.remove('progress-bar-striped');
+        progressBar.classList.add('bg-danger');
+        languageManager.currentLang.messages = ['An error occurred, please try to reload the page'];
+        document.getElementById('progress-bar-note').innerText = languageManager.currentLang.messages[0];
     }
 }
